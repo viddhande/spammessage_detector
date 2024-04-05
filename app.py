8b355ae91eeed13f70aa5340dab5f98238f54082
viddhande <116031480+viddhande@users.noreply.github.com>
2024-04-05 14:58:46 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,57 @@
+import streamlit as st
+import pickle
+import string
+from nltk.corpus import stopwords
+import nltk
+from nltk.stem.porter import PorterStemmer
+import sklearn
+
+ps = PorterStemmer()
+
+
+def transform_text(text):
+    text = text.lower()
+    text = nltk.word_tokenize(text)
+
+    y = []
+    for i in text:
+        if i.isalnum():
+            y.append(i)
+
+    text = y[:]
+    y.clear()
+
+    for i in text:
+        if i not in stopwords.words('english') and i not in string.punctuation:
+            y.append(i)
+
+    text = y[:]
+    y.clear()
+
+    for i in text:
+        y.append(ps.stem(i))
+
+    return " ".join(y)
+
+tfidf = pickle.load(open('vectorizer.pkl','rb'))
+model = pickle.load(open('model (1).pkl','rb'))
+
+st.title("SMS Spam Detector")
+
+input_sms = st.text_area("Enter your message in the text area below and click 'Predict' to determine if it's spam or not.")
+
+if st.button('Predict'):
+
+    # 1. preprocess
+    transformed_sms = transform_text(input_sms)
+    # 2. vectorize
+    vector_input = tfidf.transform([transformed_sms])
+    # 3. predict
+    result = model.predict(vector_input)[0]
+    # 4. Display
+    if result == 1:
+        st.header("Spam")
+        st.error("This message is classified as SPAM. Please be cautious and avoid interacting with this message.It's recommended not to click on any links or provide personal information.")
+
+    else:
+        st.header("Not Spam")
